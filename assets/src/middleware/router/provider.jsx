36da00a8efe2dcccd3681c2ef139543668d21924
flo611 <florian.sabine911@outlook.fr>
2023-10-30 15:14:45 +0100

    udpate

--- assets/src/middleware/router/provider.jsx
+++ assets/src/middleware/router/provider.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../../pages/home/index"
-
+import Services from "../../pages/services";
 
 export const Provider = createBrowserRouter([
     {
       path: "/",
       element:  <Home />
+    },
+    {
+      path: "/services",
+      element:  <Services />
     },
 ]);
