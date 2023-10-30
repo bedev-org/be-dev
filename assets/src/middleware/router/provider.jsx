c0f89aa7e315657d5c8954be3f0b7037aff3b939
7AiRzzZ <maxencedpr@icloud.com>
2023-10-30 15:53:22 +0100

    contact controller

--- assets/src/middleware/router/provider.jsx
+++ assets/src/middleware/router/provider.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
-import Home from "../../pages/home/index"
-
+import Home from "../../pages/home/index";
+import Contact from "../../pages/contact";
 
 export const Provider = createBrowserRouter([
-    {
-      path: "/",
-      element:  <Home />
-    },
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
 ]);
