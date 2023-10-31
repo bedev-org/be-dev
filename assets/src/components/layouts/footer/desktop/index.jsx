ae66f06def123eabf902f4d4f905d97b2faeac13
newblow <bollogmx@gmail.com>
2023-10-31 11:25:06 +0100

    update

--- assets/src/components/layouts/footer/desktop/index.jsx
+++ assets/src/components/layouts/footer/desktop/index.jsx
@@ -3,7 +3,157 @@ import React from "react";
 const footerDesktop = () => {
   return (
     <>
-      <div>footer desktop</div>
+      <footer class="text-gray-400 bg-zinc-900 body-font">
+        <div class="container px-5 py-20 mx-auto">
+          <div class="flex flex-wrap md:text-left text-center order-first">
+            <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+              <h2 class="title-font font-medium text-white tracking-widest text-sm mb-3">
+                Menu
+              </h2>
+              <nav class="list-none mb-10">
+                <li>
+                  <a class="text-gray-400 hover:text-red">Accueil</a>
+                </li>
+                <li>
+                  <a class="text-gray-400 hover:text-white">Services</a>
+                </li>
+                <li>
+                  <a class="text-gray-400 hover:text-white">Pack</a>
+                </li>
+                <li>
+                  <a class="text-gray-400 hover:text-white">Contact</a>
+                </li>
+              </nav>
+            </div>
+            <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+              <h2 class="title-font font-medium text-white tracking-widest text-sm mb-3">
+                Packs
+              </h2>
+              <nav class="list-none mb-10">
+                <li>
+                  <a class="text-gray-400 hover:text-white">Pack Free</a>
+                </li>
+                <li>
+                  <a class="text-gray-400 hover:text-white">Pack Start</a>
+                </li>
+                <li>
+                  <a class="text-gray-400 hover:text-white">Pack Pro</a>
+                </li>
+                <li>
+                  <a class="text-gray-400 hover:text-white">Pack Expert</a>
+                </li>
+              </nav>
+            </div>
+            <div className="lg:w-2/4">
+              <form action="" className="">
+                <input
+                  type="text"
+                  class="bg-gray-700 text-gray-200 border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-yellow-300 transition ease-in-out duration-150 md:w-[48%] mr-[2%]"
+                  placeholder="Nom"
+                />
+                <input
+                  type="text"
+                  class="bg-gray-700 text-gray-200 border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-yellow-500 transition ease-in-out duration-150 w-full md:w-[48%] ml-[2%]"
+                  placeholder="Prenom"
+                />
+                <input
+                  type="text"
+                  class="bg-gray-700 text-gray-200 border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-yellow-500 transition ease-in-out duration-150 md:w-[48%] mr-[2%]"
+                  placeholder="Email"
+                />
+                <input
+                  type="text"
+                  class="bg-gray-700 text-gray-200 border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:yellow-teal-500 transition ease-in-out duration-150 w-full md:w-[48%] ml-[2%]"
+                  placeholder="Numero"
+                />
+              </form>
+              <textarea
+                name="message"
+                class="bg-gray-700 text-gray-200 border-0 rounded-md p-2 mb-auto md:mb-auto md:w-full md:h-auto md:min-h-[100px] md:max-h-[100px] md:flex-grow md:flex-shrink md:flex-auto focus:bg-gray-md:focus:outline-none:focus:ring-blue-md:focus:border-transparent transition ease-in-out duration-fastest"
+                placeholder="Message"
+              ></textarea>
+              <div className="flex flex-row justify-center pb-6">
+                <button
+                  type="submit"
+                  class=" font-bold py-2 px-4 rounded-md mt-4 hover:bg-yellow-300 hover:text-white hover:to-blue-300 transition ease-in-out duration-150 "
+                >
+                  Envoyer
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div class="bg-yellow-300 bg-opacity-75">
+            <div class="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col font-extrabold text-white text-center">
+            © B E D E V - 2 0 2 4
+              <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+                <a class="text-white">
+                  <svg
+                    fill="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    class="w-5 h-5"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                  </svg>
+                </a>
+                <a class="ml-3 text-white">
+                  <svg
+                    fill="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    class="w-5 h-5"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                  </svg>
+                </a>
+                <a class="ml-3 text-white">
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    class="w-5 h-5"
+                    viewBox="0 0 24 24"
+                  >
+                    <rect
+                      width="20"
+                      height="20"
+                      x="2"
+                      y="2"
+                      rx="5"
+                      ry="5"
+                    ></rect>
+                    <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                  </svg>
+                </a>
+                <a class="ml-3 text-white">
+                  <svg
+                    fill="currentColor"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="0"
+                    class="w-5 h-5"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      stroke="none"
+                      d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
+                    ></path>
+                    <circle cx="4" cy="4" r="2" stroke="none"></circle>
+                  </svg>
+                </a>
+              </span>
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
   );
 };
