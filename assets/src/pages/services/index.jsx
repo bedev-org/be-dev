3a713bef3e664f50848d5f1f77d0b2761d33a0d6
RedBarz <barzcorp.fx@gmail.com>
2023-10-31 15:02:25 +0100

    update

--- assets/src/pages/services/index.jsx
+++ assets/src/pages/services/index.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
-const Services=()=>{
-    return(
-        <>
-        <h1>Hello serivces</h1>
-        </>
-    );
+const Services = () => {
+  return (
+    <body className="">
+      <h1>
+        Location nom de domaine <br />
+        location DB : standard, medium et premium <br />
+        location serveur : standard medium et premium
+      </h1>
+    </body>
+  );
 };
 
 export default Services;
