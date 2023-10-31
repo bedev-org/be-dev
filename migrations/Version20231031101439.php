8b4c08422aee76990e8b975877dbc582c59033cb
NeoSkyLand <lucasporet.reel@gmail.com>
2023-10-31 12:00:49 +0100

    update

--- migrations/Version20231031101439.php
+++ migrations/Version20231031101439.php
@@ -24,6 +24,7 @@ final class Version20231031101439 extends AbstractMigration
         $this->addSql('CREATE TABLE location_database (id INT AUTO_INCREMENT NOT NULL, username_database VARCHAR(255) DEFAULT NULL, password_database VARCHAR(255) DEFAULT NULL, host_database VARCHAR(255) DEFAULT NULL, port_database VARCHAR(255) DEFAULT NULL, name_database VARCHAR(255) DEFAULT NULL, stockage_database VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE location_domain (id INT AUTO_INCREMENT NOT NULL, name_domain VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE location_server (id INT AUTO_INCREMENT NOT NULL, username_server VARCHAR(255) DEFAULT NULL, password_server VARCHAR(255) DEFAULT NULL, host_server VARCHAR(255) DEFAULT NULL, port_server VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE refresh_tokens (id INT AUTO_INCREMENT NOT NULL, refresh_token VARCHAR(128) NOT NULL, username VARCHAR(255) NOT NULL, valid DATETIME NOT NULL, UNIQUE INDEX UNIQ_9BACE7E1C74F2195 (refresh_token), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
 
     }
 
@@ -34,6 +35,7 @@ final class Version20231031101439 extends AbstractMigration
         $this->addSql('DROP TABLE location_database');
         $this->addSql('DROP TABLE location_domain');
         $this->addSql('DROP TABLE location_server');
+        $this->addSql('DROP TABLE refresh_tokens');
 
     }
 }
