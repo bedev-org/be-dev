3740d8e79030adc720c782f80f65564b16b93baf
nobody932 <haykel93370@gmail.com>
2023-10-31 11:13:47 +0100

    update

--- assets/src/middleware/router/provider.jsx
+++ assets/src/middleware/router/provider.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
-import Home from "../../pages/home/index"
-
+import Home from "../../pages/home/index";
+import Login from "../../pages/login";
+import Register from "../../pages/register";
 
 export const Provider = createBrowserRouter([
-    {
-      path: "/",
-      element:  <Home />
-    },
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
 ]);
