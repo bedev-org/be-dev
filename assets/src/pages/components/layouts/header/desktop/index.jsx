daac333bce14fa2b7e900a7a2e88bda0399344df
RedBarz <barzcorp.fx@gmail.com>
2023-10-31 13:37:20 +0100

    add navbar

--- assets/src/pages/components/layouts/header/desktop/index.jsx
+++ assets/src/pages/components/layouts/header/desktop/index.jsx
@@ -1,16 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./styles.css"
+import "./styles.css";
 
 const NavbarDesktop = () => {
   return (
-    <nav className="w-screen h-[70px] max-lg:hidden bg-yellow-300/40 backdrop-blur-xl rounded-b-lg shadow-md shadow-yellow-300/30">
+    <nav className="w-screen h-[70px] max-lg:hidden bg-yellow-300/50 backdrop-blur-xl rounded-b-lg shadow-md shadow-yellow-300/30">
       <div className="w-full h-full flex justify-around items-center">
-        <div><Link className="btn-nav-mobile">Accueil</Link></div>
-        <div><Link className="btn-nav-mobile">Contact</Link></div>
-        <div><Link className="btn-nav-mobile">Connexion</Link></div>
-        <div><Link className="btn-nav-mobile">Inscription</Link></div>
-        <div><Link className="btn-nav-mobile">BeDev</Link></div>
+        <div>
+          <Link className="btn-nav-mobile">Accueil</Link>
+        </div>
+        <div>
+          <Link className="btn-nav-mobile">Services</Link>
+        </div>
+        <div>
+          <Link className="btn-nav-mobile">Contact</Link>
+        </div>
+
+        <div>
+          <Link className="btn-nav-mobile">(mettre logo)</Link>
+        </div>
+        <div>
+          <Link className="btn-nav-mobile">Profil</Link>
+        </div>
+        <div>
+          <Link className="btn-nav-mobile">Connexion</Link>
+        </div>
+        <div>
+          <Link className="btn-nav-mobile">Inscription</Link>
+        </div>
       </div>
     </nav>
   );
