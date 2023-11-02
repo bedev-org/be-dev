9b5eb11d176c36c18b624858b1b15cebbdeb0464
flo611 <florian.sabine911@outlook.fr>
2023-11-02 09:19:40 +0100

    udapte sérializer

--- assets/src/pages/services/index.jsx
+++ assets/src/pages/services/index.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
 
-const Services=()=>{
-    return(
-        <>
-        <h1>Hello serivces</h1>
-        </>
-    );
+
+import axios from "axios";
+
+const Services = () => {
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+  
+    axios.get("/serialize-user") // Utilisez l'URL appropriée pour accéder à l'action Symfony
+      .then((response) => {
+        setUserData(response.data.user);
+      })
+      .catch((error) => {
+        console.error("Erreur lors de la récupération des données de l'utilisateur", error);
+      });
+  }, []);
+
+  return (
+    <>
+      <h1>Hello services</h1>
+      {userData && (
+        <div className="bg-red-800">
+          <h2>User Data</h2>
+          <pre>{JSON.stringify(userData, null, 2)}</pre>
+        </div>
+      )}
+    </>
+  );
 };
-
 export default Services;
