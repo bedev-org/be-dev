af200f94c127b4f7cf5499e8f18db25464b840bb
sek771 <sackos77100@gmail.com>
2023-11-02 15:51:20 +0100

    update

--- assets/src/pages/dashboard/index.jsx
+++ assets/src/pages/dashboard/index.jsx
@@ -1,13 +1,6 @@
 import React from "react";
 
 const Dashboard = () => {
-  return (
-    <>
-      <h1 className="font-bold w-full flex flex-row text-green-500 justify-center text-2xl bg-black">
-        Hello Dashboard !!!
-      </h1>
-    </>
-  );
+  return <></>;
 };
-
 export default Dashboard;
