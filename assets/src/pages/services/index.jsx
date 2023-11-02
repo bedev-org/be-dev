ac2bac1f43db6637f9905e396153c5bc2d0ef5f6
flo611 <florian.sabine911@outlook.fr>
2023-11-02 16:47:28 +0100

    udpate axios

--- assets/src/pages/services/index.jsx
+++ assets/src/pages/services/index.jsx
@@ -5,10 +5,10 @@ import axios from "axios";
 
 const Services = () => {
   const [userData, setUserData] = useState(null);
+  const [testData, setTestData] = useState([]);
 
   useEffect(() => {
-  
-    axios.get("/serialize-user") // Utilisez l'URL appropriée pour accéder à l'action Symfony
+    axios.get("/serialize-user")
       .then((response) => {
         setUserData(response.data.user);
       })
@@ -16,6 +16,16 @@ const Services = () => {
         console.error("Erreur lors de la récupération des données de l'utilisateur", error);
       });
   }, []);
+
+  const locationdomain = () => {
+    axios.get("http://localhost:8000/api/location_domains")
+      .then((response) => {
+        setTestData(response.data); // Utilisez response.data au lieu de response
+      })
+      .catch((error) => {
+        console.error("Erreur lors de la récupération des données de l'utilisateur", error);
+      });
+  }
 
   return (
     <>
@@ -26,7 +36,27 @@ const Services = () => {
           <pre>{JSON.stringify(userData, null, 2)}</pre>
         </div>
       )}
+
+      <div className="grid grid-cols-3">
+        {testData.map((testData) => (
+          <div key={testData['@id']}>
+            <h2>Titre : {testData.id}</h2>
+            <h2>Password: {testData.name_domain}</h2>
+          </div>
+        ))}
+      </div>
+
+      <div>
+        <button
+          type="button"
+          className=""
+          onClick={locationdomain}
+        >
+          locationdomain
+        </button>
+      </div>
     </>
   );
 };
+
 export default Services;
