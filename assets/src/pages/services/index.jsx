b8d0b51a30563f8258114cf8cb8650b754d505d0
flo611 <florian.sabine911@outlook.fr>
2023-11-02 20:25:55 +0100

    working axios

--- assets/src/pages/services/index.jsx
+++ assets/src/pages/services/index.jsx
@@ -7,6 +7,9 @@ const Services = () => {
   const [userData, setUserData] = useState(null);
   const [testData, setTestData] = useState([]);
   const[database, setDatabase]= useState([])
+  const[newDomain,setNewDomain]= useState({
+  nameDomain: "",
+});
 
   useEffect(() => {
     axios.get("/serialize-user")
@@ -14,7 +17,7 @@ const Services = () => {
         setUserData(response.data.user);
       })
       .catch((error) => {
-        console.error("Erreur lors de la récupération des données de l'utilisateur", error);
+        console.error("Erreur récupération des données de l'utilisateur", error);
       });
       locationdomain();
       locationdatabase();
@@ -34,19 +37,43 @@ const Services = () => {
 
     axios.get("http://localhost:8000/api/location_databases")
     .then((response) => {
-      setTestData(response.data["hydra:member"]);
+      setDatabase(response.data["hydra:member"]);
     })
     .catch((error) => {
       console.error("Erreur lors de la récupération des données de locationdomain", error);
     });
   }
 
+  const addDomain =()=>{
+
+    axios.post("http://localhost:8000/api/location_domains",newDomain)
+    
+    .then(() => {
+      alert("Domaine crée !");
+
+      window.location.reload();
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  }
+
+  const config = {
+    headers: {
+      Authorization: `bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE2OTgyMzU5MzYsImV4cCI6MTY5ODIzOTUzNiwicm9sZXMiOlsiUk9MRV9BRE1JTiIsIlJPTEVfVVNFUiJdLCJ1c2VybmFtZSI6ImFkbWluQG5hdHVyb3BpZWQuZnIifQ.a6uK7VniuY86lN33Fg_MAFrqchKxJA27EXMPRzuQWbBeCtnki1A4pNyfy5Hnf3eJN0FSkLdhZcehLBJ_lXm6Kn2ajht8uZpLFodKG_4-0FVCBAOs5CKEdV7tpDY5IFZzTpbqDDHHzyUfdBVbkX2xuWtJsEe3nHkVJSUtkTFgt6wvZ6-M3zo_Wt1obyGx0_v1uXTRC5XtAPPIz39AOvARsVeXK6kDEdbTX8svNVWxsECmDG6C3HPzieBvqSNXjosykybtCQqU94-IwNIpBeUyresetHNkxTRqdOfgkU14RMV-znFJD1OJE_EaVa-EvELDuu6u7xyWSvKjetPF-vK3VQ`,
+    },
+  };
+  const domain = {
+    nameDomain: newDomain.nameDomain,
+ 
+  };
+
   return (
     <>
       <h1>Hello services</h1>
       {userData && (
-        <div className="bg-red-800">
-          <h2>User Data</h2>
+        <div className="bg-neutral-400">
+          <h2 className="flex justify-center bg-red-500">User Data</h2>
           <pre>{JSON.stringify(userData, null, 2)}</pre>
         </div>
       )}
@@ -63,14 +90,35 @@ const Services = () => {
 
       <h1 className="w-full flex justify-center bg-cyan-500"> Database</h1>
       <div className="grid grid-cols-3">
-        {database.map((database) => (
+        {database.slice(0,10).map((database) => (
           <div key={database['@id']}>
-            <h2>Titre : {database.user_name}</h2>
-            <h2>Password: {database.name_domain}</h2>
+            <h1>Nom : {database.name_database}</h1>
+            <h2>Titre : {database.username_database}</h2>
+            <h2>Password: {database.name_database}</h2>
             <h3>Port: {database.port_database}</h3>
+            <h4>Capacité :{database.stockage_database} Go</h4>
           </div>
         ))}
   
+      </div>
+<div>
+  <h1 className="w-full flex justify-center bg-green-600">Création de domaine </h1>
+      <form action="" >
+      <label htmlFor="nameDomain">Nom de domaine</label>
+      <input  type="text"
+            id="nameDomain"
+            name="nameDomain"
+            value={newDomain.nameDomain}
+            onChange={(event) =>
+                  setNewDomain({ ...newDomain, nameDomain: event.target.value })
+                } />
+      </form>
+      <button
+          type="button"
+          onClick={addDomain}
+        >
+          Envoyer
+        </button>
       </div>
     </>
   );
