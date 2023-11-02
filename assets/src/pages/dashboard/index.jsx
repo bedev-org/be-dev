f14e8a9b7763ac857a8c39abc2507a5e2165cf92
gourdy <gourdygautier@gmail.com>
2023-11-02 16:13:57 +0100

    update

--- assets/src/pages/dashboard/index.jsx
+++ assets/src/pages/dashboard/index.jsx
@@ -6,142 +6,37 @@ const Dashboard = () => {
   return (
     <section class="whitebedev-text body-font blackbedev">
       <Navbar />
-      <section>
-        <div class="container px-5 py-24 mx-auto flex flex-wrap">
-          <div class="flex flex-wrap w-full">
-            <div class="lg:w-2/5 md:w-1/2 md:pr-10 md:py-6">
-              <div class="flex relative pb-12">
-                <div class="h-full w-10 absolute inset-0 flex items-center justify-center"></div>
-                <div class="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center text-white relative z-10">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    class="w-5 h-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
-                  </svg>
-                </div>
-                <div class="flex-grow pl-4">
-                  <h2 class="font-medium title-font text-sm yellowbedev-text mb-1 tracking-wider">
-                    Nom
-                  </h2>
-                  <p class="leading-relaxed">
-                    blabla
-                  </p>
-                </div>
-              </div>
-              <div class="flex relative pb-12">
-                <div class="h-full w-10 absolute inset-0 flex items-center justify-center"></div>
-                <div class="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center text-white relative z-10">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    class="w-5 h-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-                  </svg>
-                </div>
-                <div class="flex-grow pl-4">
-                  <h2 class="font-medium title-font text-sm yellowbedev-text mb-1 tracking-wider">
-                    Prénom
-                  </h2>
-                  <p class="leading-relaxed">
-                    blabla
-                  </p>
-                </div>
-              </div>
-              <div class="flex relative pb-12">
-                <div class="h-full w-10 absolute inset-0 flex items-center justify-center"></div>
-                <div class="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center text-white relative z-10">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    class="w-5 h-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <circle cx="12" cy="5" r="3"></circle>
-                    <path d="M12 22V8M5 12H2a10 10 0 0020 0h-3"></path>
-                  </svg>
-                </div>
-                <div class="flex-grow pl-4">
-                  <h2 class="font-medium title-font text-sm yellowbedev-text mb-1 tracking-wider">
-                    blabla@blabla.com
-                  </h2>
-                  <p class="leading-relaxed">
-                    Coloring book nar whal glossier master cleanse umami. Salvia
-                    +1 master cleanse blog taiyaki.
-                  </p>
-                </div>
-              </div>
-              <div class="flex relative pb-12">
-                <div class="h-full w-10 absolute inset-0 flex items-center justify-center"></div>
-                <div class="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center text-white relative z-10">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    class="w-5 h-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
-                    <circle cx="12" cy="7" r="4"></circle>
-                  </svg>
-                </div>
-                <div class="flex-grow pl-4">
-                  <h2 class="font-medium title-font text-sm yellowbedev-text mb-1 tracking-wider">
-                    Numéro de téléphone
-                  </h2>
-                  <p class="leading-relaxed">
-                    89456123048951628951602
-                  </p>
-                </div>
-              </div>
-              <div class="flex relative">
-                <div class="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center text-white relative z-10">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    class="w-5 h-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
-                    <path d="M22 4L12 14.01l-3-3"></path>
-                  </svg>
-                </div>
-                <div class="flex-grow pl-4">
-                  <h2 class="font-medium title-font text-sm yellowbedev-text mb-1 tracking-wider">
-                    Société
-                  </h2>
-                  <p class="leading-relaxed">
-                    blabla
-                  </p>
-                </div>
-              </div>
+
+      <section class="text-gray-600 body-font">
+        <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+          <img
+            src="https://source.unsplash.com/random/500x400/?man "
+            alt=""
+            class="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2"
+          />{" "}
+          <div class="text-center lg:w-2/3 w-full">
+            <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+              Microdosing synth tattooed vexillologist
+            </h1>
+            <p class="mb-8 leading-relaxed">
+              Meggings kinfolk echo park stumptown DIY, kale chips beard
+              jianbing tousled. Chambray dreamcatcher trust fund, kitsch vice
+              godard disrupt ramps hexagon mustache umami snackwave tilde
+              chillwave ugh. Pour-over meditation PBR&B pickled ennui celiac
+              mlkshk freegan photo booth af fingerstache pitchfork.
+            </p>
+            <div class="flex justify-center">
+              <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                Button
+              </button>
+              <button class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
+                Button
+              </button>
             </div>
-            <img
-              src="https://source.unsplash.com/random/500x400/?man "
-              alt=""
-              class="lg:w-3/5 md:w-1/2 object-cover object-center rounded-lg md:mt-0 mt-12"
-            />{" "}
           </div>
         </div>
       </section>
+
       <section class="whitebedev-text body-font overflow-hidden">
         <div class="container px-5 py-24 mx-auto">
           <div class="-my-8 divide-y-2 divide-gray-100">
