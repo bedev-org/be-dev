03b300e423da309f2c55c4d32c8a418943c8e204
NeoSkyLand <lucasporet.reel@gmail.com>
2023-11-03 09:42:14 +0100

    update

--- assets/src/pages/components/layouts/header/desktop/index.jsx
+++ assets/src/pages/components/layouts/header/desktop/index.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 import "./styles.css";
 
 const NavbarDesktop = () => {
+
+  const [userData, setUserData] = useState(null);
+  useEffect(() => {
+      axios.get("/serialize-user")
+        .then((response) => {
+          setUserData(response.data.user);
+        })
+        .catch((error) => {
+          console.error("Erreur récupération des données de l'utilisateur", error);
+        });
+  }, []);
+
+
   return (
+    <>
+    {userData && (    <pre>{JSON.stringify(userData, null, 2)}</pre>   )}
     <nav className="w-screen h-[70px] max-lg:hidden yellowbedev backdrop-blur-xl rounded-b-lg shadow-md shadow-yellow-300/30">
       <div className="w-full h-full flex justify-around items-center">
         <div>
@@ -19,6 +35,7 @@ const NavbarDesktop = () => {
         <div>
           <Link className="btn-nav-mobile">(mettre logo)</Link>
         </div>
+      
         <div>
           <Link className="btn-nav-mobile">Profil</Link>
         </div>
@@ -30,6 +47,7 @@ const NavbarDesktop = () => {
         </div>
       </div>
     </nav>
+    </>
   );
 };
 export default NavbarDesktop;
--- assets/src/pages/services/index.jsx
+++ assets/src/pages/services/index.jsx
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-import React,{useEffect,useState} from "react";
-
-
-import axios from "axios";
-
-const Services = () => {
-  const [userData, setUserData] = useState(null);
-
-  useEffect(() => {
-  
-    axios.get("/serialize-user") // Utilisez l'URL appropriée pour accéder à l'action Symfony
-      .then((response) => {
-        setUserData(response.data.user);
-      })
-      .catch((error) => {
-        console.error("Erreur lors de la récupération des données de l'utilisateur", error);
-      });
-  }, []);
-
-  return (
-    <>
-      <h1>Hello services</h1>
-      {userData && (
-        <div className="bg-red-800">
-          <h2>User Data</h2>
-          <pre>{JSON.stringify(userData, null, 2)}</pre>
-        </div>
-      )}
-    </>
-=======
 import React from "react";
 import PriceCard from "../components/layouts/priceCard/priceCard";
 import FavoriteCard from "../components/layouts/priceCard/favoriteCard";
@@ -82,7 +51,6 @@ const Services = () => {
         </div>
       </section>
     </body>
->>>>>>> 069b6d7637329076c57166ffc2727a9028b7b5f0
   );
 };
 export default Services;
