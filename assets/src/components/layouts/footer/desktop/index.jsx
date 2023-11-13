e7c7f31f29793f8373ef70cf2b5262d36ac22657
flo611 <florian.sabine911@outlook.fr>
2023-11-13 19:43:37 +0100

    udpate page services + footer

--- assets/src/components/layouts/footer/desktop/index.jsx
+++ assets/src/components/layouts/footer/desktop/index.jsx
@@ -3,53 +3,54 @@ import React from "react";
 const footerDesktop = () => {
   return (
     <>
-      <footer class="text-gray-400 bg-zinc-900 body-font bottom-0">
-        <div class=" px-5 py-20 flex flex-row justify-center ">
+      <footer class="text-black yellowbedev body-font bottom-0">
+        <div class=" py-20 flex flex-row justify-center ">
           <div class="lg:flex flex-row">
             <div class=" w-full ">
-              <h2 class="title-font font-medium text-white tracking-widest text-sm mb-3">
-                Address
+              <h2 class="title-font font-medium text-black tracking-widest text-sm mb-3">
+                Addresse
               </h2>
               <nav class="list-none mb-10  ">
                 <li>
-                  <a class="text-gray-400 hover:text-white">
-                    30 Bd du Chevalier Bayard <br />
+                  <a class="text-black hover:text-black whitespace-nowrap ">
+                    30 Bd du Chevalier Bayard <br /></a>
+                    <a class="text-black hover:text-black ">
                     77100 Meaux, France
+                    </a>
+                </li>
+                <li>
+                  <a class="text-black hover:text-black whitespace-nowrap">
+                    contact@be-dev.org
                   </a>
                 </li>
                 <li>
-                  <a class="text-gray-400 hover:text-white">
-                    contact@grumiaux-portfolio.fr
-                  </a>
-                </li>
-                <li>
-                  <a class="text-gray-400 hover:text-white">06 56 71 62 84 </a>
+                  <a class="text-black hover:text-black">+33 (0)3 84 76 52 44 </a>
                 </li>
               </nav>
             </div>
 
             <div class=" w-full lg:px-40 ">
-              <h2 class="title-font font-medium text-white tracking-widest text-sm mb-3">
+              <h2 class="title-font font-medium text-black tracking-widest text-sm mb-3">
                 Menu
               </h2>
               <nav class="list-none mb-10">
                 <li>
-                  <a href="/" class="text-gray-400 hover:text-red">
+                  <a href="/" class="text-black hover:text-red">
                     Accueil
                   </a>
                 </li>
                 <li>
-                  <a href="/services" class="text-gray-400 hover:text-white">
+                  <a href="/services" class="text-black hover:text-black">
                     Services
                   </a>
                 </li>
                 <li>
-                  <a href="/profil" class="text-gray-400 hover:text-white">
+                  <a href="/profil" class="text-black hover:text-black">
                     Profil
                   </a>
                 </li>
                 <li>
-                  <a href="/contact" class="text-gray-400 hover:text-white">
+                  <a href="/contact" class="text-black hover:text-black">
                     Contact
                   </a>
                 </li>
@@ -57,18 +58,22 @@ const footerDesktop = () => {
             </div>
             <div class="w-full ">
               <nav class="list-none mb-10">
+              <h2 class="title-font font-medium text-black tracking-widest text-sm mb-3">
+                RGPD
+              </h2>
                 <li>
-                  <a href="/" class="text-gray-400 ">
+                  <a href="/" class="text-black ">
                     Mentions légales
                   </a>
                 </li>
                 <li>
-                  <a href="/" class="text-gray-400 hover:text-white">
-                    politique de confidentialité
+                  <a href="/confidentialite" class="text-black hover:text-black whitespace-nowrap">
+                    Politique de confidentialité
+                  
                   </a>
                 </li>
                 <li>
-                  <a href="/" class="sm:hidden text-gray-400 hover:text-white">
+                  <a href="/" class="sm:hidden text-black hover:text-black">
                     © B E D E V - 2 0 2 4
                   </a>
                 </li>
--- assets/src/pages/services/index.jsx
+++ assets/src/pages/services/index.jsx
@@ -19,22 +19,39 @@ const Services = () => {
     <>
       <Navbar />
       <body className="blackbedev">
-        <section className="text-white body-font overflow-hidden">
-          <div className="container px-5 py-24 mx-auto">
-            <div className="flex flex-col text-center w-full mb-20 ">
-              <h1 className="sm:text-4xl text-3xl font-medium title-font mb-2 text-white">
-                Nos services
-              </h1>
-              <p className="lg:w-2/3 mx-auto leading-relaxed text-base text-gray-400">
-                Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical.
-              </p>
-            </div>
+      <section className="text-white body-font overflow-hidden">
+  <div
+    style={{
+      backgroundImage: 'url("https://media.istockphoto.com/id/1296993493/fr/photo/centre-de-donn%C3%A9es-de-la-salle-des-serveurs-sauvegarde-exploitation-mini%C3%A8re-h%C3%A9bergement.jpg?s=2048x2048&w=is&k=20&c=CQLv0ZPJmvqqSwpxtc68g-Jw3wjjC1HM0OZ8GrARvtU=")',
+      backgroundSize: 'cover',
+      backgroundPosition: 'top',
+    }}
+    className="flex justify-center items-center border rounded-lg p-5 md:p-16 lg:p-28 h-96"
+  >
+   <h2 className="sm:text-4xl text-3xl font-medium title-font mt-20 text-black bg-gradient-to-r from-yellow-400 to-yellow-600 p-6 rounded-md shadow-lg">
+        Nos services
+      </h2>
+  </div>
+  <div className="container px-5 py-6 mx-auto">
+    <div className="flex flex-col text-center w-full ">
+      
+      <p className="lg:w-2/3 mx-auto leading-relaxed text-base text-gray-400 py-6">
+        🚀 Propulsez votre entreprise vers de nouveaux sommets avec notre offre exclusive de location de base de données et de serveurs, accompagnée d'un nom de domaine sur mesure ! <br />
+        ✨Boostez votre efficacité opérationnelle : Accédez à des bases de données performantes et des serveurs ultrarapides pour garantir une expérience utilisateur sans faille. <br /><br />
+        Découvrez dès maintenant notre offre exclusive de location de base de données et de serveurs, avec un nom de domaine à la clé ! 🔗💻
+      </p>
+    </div>
+  </div>
             <div className="">
               <TitleServices title="Location nom de domaines" />
-              <div className="w-full flex flex-col lg:flex-row justify-around items-center">
-                <div className="my-5">
-                  <p className="w-[280px] lg:border-r lg:border-white/50 lg:pr-5">
-                  En optant pour la location de noms de domaine, vous pouvez bénéficier d'une flexibilité accrue. Vous pouvez choisir un nom de domaine pertinent pour une campagne spécifique, un lancement de produit, ou d'entreprise dans les différents choix proposés. Testez l'égibilité !!
+              <div className="w-full flex flex-col lg:flex-row justify-around items-center  lg:items-start">
+                <div className="my-10">
+                  <p className="w-[400px] lg:border-r lg:border-white/50 lg:pr-16 h-[600px] leading-6 py-40  ">
+                    En optant pour la location de nom de domaine, vous pouvez
+                    bénéficier d'une flexibilité accrue. Vous pouvez choisir un
+                    nom de domaine pertinent pour une campagne spécifique, un
+                    lancement de produit, ou d'entreprise dans les différents
+                    choix proposés. Testez l'égibilité !!
                   </p>
                 </div>
                 <PriceCard
@@ -61,8 +78,14 @@ const Services = () => {
               <TitleServices title="Location base de données" />
               <div className=" w-full flex flex-col justify-center items-center lg:flex-row lg:justify-around border-b border-white/20 pb-10">
                 <div className="my-5">
-                  <p className="w-[280px] lg:border-r lg:border-white/50 lg:pr-5">
-                  La location de bases de données che be-dev vous offrrira un accès rapide et ciblé à des informations cruciales. Que ce soit pour élargir sa clientèle, optimiser ses campagnes marketing ou améliorer sa compréhension du marché, la location de bases de données présente des avantages significatifs pour vous. Alors n'hésistez pas à regarder nos offres !!
+                  <p className="w-[280px] lg:border-r lg:border-white/50 lg:pr-5 ">
+                    La location de bases de données che be-dev vous offrrira un
+                    accès rapide et ciblé à des informations cruciales. Que ce
+                    soit pour élargir sa clientèle, optimiser ses campagnes
+                    marketing ou améliorer sa compréhension du marché, la
+                    location de bases de données présente des avantages
+                    significatifs pour vous. Alors n'hésistez pas à regarder nos
+                    offres !!
                   </p>
                 </div>
                 <UltimateCard
@@ -100,48 +123,39 @@ const Services = () => {
               <div className=" w-full flex flex-col justify-center items-center lg:flex-row lg:justify-around border-b border-white/20 pb-10">
                 <div className="my-5">
                   <p className="w-[280px] lg:border-r lg:border-white/50 lg:pr-5">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Praesentium ab itaque voluptates amet ullam enim ea magni
-                    labore commodi, non reprehenderit neque eos quae numquam
-                    dolores repudiandae mollitia voluptate minus!
+                  Choisissez la fiabilité et la vitesse pour propulser votre entreprise vers de nouveaux sommets. Nos serveurs de pointe offrent une expérience sans compromis, garantissant une disponibilité constante et des temps de réponse ultrarapides !
                   </p>
                 </div>
                 <UltimateCard
                   pack="Pack standard"
                   price="5€"
                   delay="/Mois"
-                  option1="option numéro 1"
-                  option2="option numéro 2"
-                  option3="option numéro 3"
-                  option4="option numéro 4"
-                  option5="option numéro 5"
+                  option1="1 Core"
+                  option2="1 Go RAM"
+                  option3="40 Go SSD"
                   onClick={() => AddServer("standard", 5)}
                 />
                 <FavoriteCard
                   pack="Pack medium"
                   price="10€"
                   delay="/Mois"
-                  option1="option numéro 1"
-                  option2="option numéro 2"
-                  option3="option numéro 3"
-                  option4="option numéro 4"
-                  option5="option numéro 5"
+                  option1="2 Cores"
+                  option2="4 Go RAM"
+                  option3="80 Go SSD"
                   onClick={() => AddServer("medium", 10)}
                 />
                 <UltimateCard
                   pack="Pack premium"
                   price="15€"
                   delay="/Mois"
-                  option1="option numéro 1"
-                  option2="option numéro 2"
-                  option3="option numéro 3"
-                  option4="option numéro 4"
-                  option5="option numéro 5"
+                  option1="4 Cores"
+                  option2="8 Go RAM"
+                  option3="120 Go SSD"
                   onClick={() => AddServer("premium", 15)}
                 />
               </div>
             </div>
-          </div>
+         
         </section>
       </body>
       <Footer />
